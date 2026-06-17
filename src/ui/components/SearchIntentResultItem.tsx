0cf8aa71f33195456a32c5641ba7c549f429a9ed
MAA-98 <marek.a.kurczynski@gmail.com>
2026-06-18 00:37:48 +0100

    Better search UI

--- src/ui/components/SearchIntentResultItem.tsx
+++ src/ui/components/SearchIntentResultItem.tsx
@@ -11,21 +11,12 @@ export function SearchIntentResultItem({ intent, isSelected = false }: Props) {
   return (
     <Box
       flexDirection="column"
-      borderStyle={isSelected ? 'round' : undefined}
       paddingX={isSelected ? 1 : 0}
-      paddingY={isSelected ? 0 : 0}
-      marginBottom={1}
     >
       <Text color={isSelected ? 'green' : undefined} bold={isSelected}>
         {isSelected ? '▶ ' : '  '}
         {intent.id} : {intent.shortDesc}
       </Text>
-
-      {isSelected && (
-        <Box marginTop={0} marginLeft={2} flexDirection="column">
-          <Text dimColor>{intent.longDesc}</Text>
-        </Box>
-      )}
     </Box>
   );
 }
